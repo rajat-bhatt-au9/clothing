e8d43b00b60d545ae641a7ab5102a95061d051f6
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2021-01-10 22:54:59 +0530

    update our app to redirect to home when signed in

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -33,8 +33,8 @@ const Header = ({ currentUser })=>(
     </div>
 )
 
-const mapStateTpProps = state =>({
+const mapStateToProps = state =>({
     currentUser: state.user.currentUser
 })
 
-export default connect(mapStateTpProps) (Header);
+export default connect(mapStateToProps) (Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,7 @@
 import { ReactReduxContext } from "react-redux";
 import { combineReducers } from 'redux';
 
-import userReducer from './user/user-reducer';
+import userReducer from './user/user.reducer';
 
 
 

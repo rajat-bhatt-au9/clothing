8b3f9cee6792bdbbe696a42c843402930079eda6
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2021-01-14 17:59:36 +0530

    some minor chnages

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -1,69 +1,89 @@
-import React, { Component } from 'react';
-import FormInput from '../form-input/FormInput';
-import CustomButton from '../custom-button/CustomButton';
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
-import './signin.scss';
+import React, { Component } from "react";
+import FormInput from "../form-input/FormInput";
+import CustomButton from "../custom-button/CustomButton";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
+import "./signin.scss";
 
-class SignIn extends React.Component{
-    constructor(props){
-        super(props);
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state={
-            email:'',
-            password:'',
-            loginError:''
-        }
-    }
+    this.state = {
+      email: "",
+      password: "",
+      loginError: "",
+    };
+  }
 
-    handleSubmit = async event =>{
-        event.preventDefault();
+  handleSubmit = async (event) => {
+    event.preventDefault();
 
-        const { email, password } = this.state;
-        try{
-            await auth.signInWithEmailAndPassword(email, password);    
-            this.setState({email:'', password:''})
-        }catch(error){ 
-            console.log(error);/* 
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(
+        error
+      ); /* 
             this.setState({loginError: error.message}) */
-            switch(error.code){
-                case 'auth/user-not-found':
-                    this.setState({loginError: 'User Not Found'})
-                    break;
-            }
-        }
-
+      switch (error.code) {
+        case "auth/user-not-found":
+          this.setState({ loginError: "User Not Found" });
+          break;
+      }
     }
+  };
 
-    handleChange= event =>{
-        const { value, name} = event.target;
+  handleChange = (event) => {
+    const { value, name } = event.target;
 
-        this.setState({[name]: value})
-    }
+    this.setState({ [name]: value });
+  };
 
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
 
-    render(){
-        return(
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
-                
-                <div className='error'>
-                    <h2> {this.state.loginError} </h2>
-                </div>
-                
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput name='email' type='email' handleChange={this.handleChange} value={this.state.email} label='Email' required />
+        <div className="error">
+          <h2> {this.state.loginError} </h2>
+        </div>
 
-                    <FormInput name='password' type='password' handleChange={this.handleChange} label='Password' value={this.state.password} required />
-                    
-                    <div className='buttons'>
-                        <CustomButton type='submit' >Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} type="button" isGoogleSignIn >Sign In With Google</CustomButton>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label="Email"
+            required
+          />
+
+          <FormInput
+            name="password"
+            type="password"
+            handleChange={this.handleChange}
+            label="Password"
+            value={this.state.password}
+            required
+          />
+
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton
+              onClick={signInWithGoogle}
+              type="button"
+              isGoogleSignIn
+            >
+              Sign In With Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
